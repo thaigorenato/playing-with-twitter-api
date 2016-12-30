cb4ace3adfeb22383026cb189c68889e20c5682d
Cloves Adriano <cloves.sousa@prognoos.com>
2016-12-30 15:58:15 -0200

    keyError resolvido

--- organize_data.py
+++ organize_data.py
@@ -1,31 +1,28 @@
 import json
+import ast 
 import pandas as pd 
 import matplotlib.pyplot as plt
-import ast 
 
 tweets_data = []
 
-tweets_file = open('teste.txt', "r")
-
-for line in tweets_file:
-    dic_string = json.dumps(line)
-    json_acceptable_string = dic_string.replace("'", "\"")
-    tweet = json.loads(json_acceptable_string)
-    tweets_data.append(tweet)
+with open('teste.txt', "r") as tweets_file:
+    tweets_data = [ast.literal_eval(line) for line in tweets_file]
 
 
+tweets = pd.DataFrame()
 
+tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
+tweets['lang'] = map(lambda tweet: tweet['lang'], tweets_data)
+tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None\
+                         else None, tweets_data)
 
+tweets_by_lang = tweets['lang'].value_counts()
 
-'''
-for line in tweets_file:
-    dic_string = json.dumps(line)
-    json_acceptable_string = dic_string.replace("'", "\"")
-    tweet = json.loads(json_acceptable_string)
-    tweets_data.append(tweet)'''
-    
-''' 
-s = "{'muffin' : 'lolz', 'foo' : 'kitty'}"
-json_acceptable_string = line.replace("'", "\"")
-d = json.loads(json_acceptable_string)
-'''
+fig, ax = plt.subplots()
+
+ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.set_xlabel('Languages', fontsize=15)
+ax.set_ylabel('Number of tweets' , fontsize=15)
+ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
+tweets_by_lang[:2].plot(ax=ax, kind='bar', color='red')
